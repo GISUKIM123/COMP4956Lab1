a5b3e548405ccca7d5b7c36ae61b65618fa6187b
Gisu Kim <sksmszhdzk@gmail.com>
2017-10-11 18:00:51 -0700

    fixed cpp version

--- Lab1_cppversion/Lab1_cppversion/Lab1.cpp
+++ Lab1_cppversion/Lab1_cppversion/Lab1.cpp
@@ -1,16 +1,36 @@
+/// <summary>
+/// Name:       Lab 0
+/// Purpose:    building multithread and each thread count a number and display it      
+/// Author:     Gisu Kim, A00959494
+/// Date:       2017-10-10
+/// Updated by: 2017-10-10
+/// Based On:   Lab01
+/// What the superviosr should know: N/A
+
 #include <iostream>
-#include <cstdlib>
 #include <stdio.h>
 #include <thread>
-#include <Windows.h>
+#include <Windows.h>  /*  Windows-specific header file  */
+#include <chrono>	/* a flexible collection of types that track time with varying degrees of precision */
+#include <string>
+#include <time.h>
+
+#define _CRT_SECURE_NO_WARNINGS
 
 using namespace std;
+using namespace std::chrono;
 
-void task1(int index){
-	while (true) {
-		cout << "hello" << "\n" << endl;
+
+/// <summary>
+/// thread function, each one counts a number till the timer is out
+/// </summary>
+void threadFuntion(system_clock::time_point timeout, int threadIndex){
+	int count = 0;
+	while (timeout > chrono::system_clock::now()) {
+		cout << count++ << endl;
 	}
 }
+
 
 int main() {
 	HANDLE hTimer = NULL;
@@ -24,27 +44,34 @@ int main() {
 	cout << "PLease enter time(sec) when you want to run threads\n" << endl;
 	cin >> second;
 	
-	//set timeout
-	liDueTIME.QuadPart = -10000000 * second;
+	/* setup time with current time*/
+	system_clock::time_point timeout = system_clock::now();
+	/* define timeout time with second that user enter and current time */
+	timeout = timeout + seconds(second);
+	
+	time_t startTime;
+	startTime = system_clock::to_time_t(system_clock::now());
+	
+	cout << "Threads start at: " << ctime(&startTime) << endl;
 
-	//create waitable timer
-	hTimer = CreateWaitableTimer(NULL, true, "WaitableTimer");
-	if (!hTimer) {
-		cout << "CreateWaitableTimer function failed\n Error: " + GetLastError() << endl;
-		return 1;
+	/* loop through and create a number of threads and let them run the function*/
+	for (int i = 0; i < numberOfthreads - 1; i++) {
+		try {
+			int index = i;
+			thread t{ threadFuntion, timeout, index};
+			t.detach();
+		}
+		catch (...) {
+			throw;
+		}
 	}
 
-	//set a timer with timeout 
-	if (!SetWaitableTimer(hTimer, &liDueTIME, 0, NULL, NULL, 0)) {
-		cout << "SetWaitableTimer function failed\n Error: " + GetLastError() << endl;
-		return 1;
-	}
+	time_t endTime;
+	endTime = system_clock::to_time_t(system_clock::now());
+	cout << "-------------------------------------------------------------------" << endl;
+	cout << "Threads end at: " << ctime(&endTime) << endl;
 
-	if (WaitForSingleObject(hTimer, INFINITE) != WAIT_OBJECT_0) {
-		cout << "WaitForSingleObject() failed, error: " + GetLastError() << endl;
-		return 1;
-	}
-
+	
 	system("pause");
 	return 0;
 }
